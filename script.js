d0d2d4e79fb17c392aeec4a3ee93e2bd1780b832
alboncho <xetres3@gmail.com>
2026-04-27 20:42:31 -0400

    agregando algoritmo FCFS

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-let tabla_algoritmo = document.querySelectorAll(".tabla-algoritmo");
+let tabla_algoritmo = document.querySelectorAll(".tablaProcesos");
+let bxModal = document.querySelector(".bxModal");
+
+bxModal.addEventListener('click', ocultar);
+
+function ocultar(e) {
+   e.currentTarget.style.display = 'none';
+}
+
+let datos;
+
 async function leerLineaPorLinea() {
    const respuesta = await fetch('datos.txt');
    const lector = respuesta.body.getReader();
@@ -23,11 +33,13 @@ async function leerLineaPorLinea() {
    let tabalDos = Math.floor(lineas.length/2);
    let tablaNumero = tablaUno;
 
+   datos = [...lineas];
+
    tabla_algoritmo.forEach(function(tabla) {
       for (let i=index; i<(index+tablaNumero); i++) {
          let tr = document.createElement('tr');
          let datos = lineas.shift();
-         for (let j=0; j<7; j++) {
+         for (let j=0; j<8; j++) {
             let td = document.createElement('td');
             if (j == 0) {
                td.textContent = (i+1);
@@ -43,25 +55,160 @@ async function leerLineaPorLinea() {
    });
 }
 
-
 leerLineaPorLinea();
 
+const algoritmo = {
+   FCFS: false,
+   Prioridad: false,
+   RR: false,
+   SJN: false,
+}
+
+function mostrarModal(e) {
+   bxModal.style.display = 'block';
+   bxModal.style.display = 'flex';
+   bxModal.style.justifyContent = 'center';
+   bxModal.style.alignItems = 'center';
+}
 
 function cambiarActive(e) {
    algoritmos.forEach(function(algoritmo) {
       algoritmo.classList.remove('active');
    })
    e.currentTarget.classList.toggle('active');
+
+   algoritmo.FCFS = false;
+   algoritmo.Prioridad = false;
+   algoritmo.RR = false;
+   algoritmo.SJN = false;
+   algoritmo[e.target.textContent] = !false;
+   textoAlgoritmo = e.target.textContent;
+}
+
+function ordenar(datos) {
+   for (let i=0; i<datos.length-1; i++) {
+      for (let j=i+1; j<datos.length; j++) {
+         if (datos[i].split(' ')[0] > datos[j].split(' ')[0]) {
+            let aux = datos[i];
+            datos[i] = datos[j];
+            datos[j] = aux;
+         }
+      }
+   }
+}
+
+function asignarPosicion(datos) {
+   for (let i=0; i<datos.length; i++) {
+      datos[i] += ` ${i+1}`;
+   }
+}
+
+function traerContenido(posicion, TEI, resultado) {
+   for (let i=0; i<resultado.length; i++) {
+
+      if (resultado[i].split(' ')[0] == posicion) {
+         if (TEI === 0) {
+            return resultado[i].split(' ')[1];
+         } else if (TEI === 1) {
+            return resultado[i].split(' ')[2];
+         } else if (TEI === 2) {
+            return resultado[i].split(' ')[3];
+         } else {
+            return resultado[i].split(' ')[4];
+         }
+      }
+   }
+}
+
+function agregarDatosATabla(resultado) {
+   tabla_algoritmo.forEach(function(tabla) {
+      let filas = tabla.children;
+      let indice = 0;
+
+      [...filas].forEach((fila) => {
+         if (fila.children.length > 1) {
+            let columnas = fila.children;
+            let TEI = 0;
+
+            [...columnas].forEach((columna) => {
+               if (columna.textContent == "") {
+                  columna.textContent = traerContenido(indice, TEI, resultado);
+                  TEI++;
+               } 
+            });
+
+         }
+         indice++;
+      });
+   });
+}
+
+let resultado = [];
+
+function calcularAlgoritmo(textoAlgoritmo) {
+   switch (textoAlgoritmo) {
+      case 'FCFS':
+         asignarPosicion(datos);
+         console.log(" asignar: ", datos);
+         ordenar(datos);
+         console.log(" ordenar: ", datos);
+
+         let tiempo = 0;
+         let proceso = datos.shift();
+
+         while (true) {
+            if (proceso.split(' ')[3] == 27) {
+               console.log(proceso);
+            }
+            if (proceso.split(' ')[0] == tiempo || proceso.split(' ')[0] <= tiempo) {
+               tiempo += parseInt(proceso.split(' ')[1]);
+
+               let tiempo_inicial = parseInt(proceso.split(' ')[0]);
+               let tiempo_ejecucion = parseInt(proceso.split(' ')[1]);
+               let T = `${tiempo - tiempo_inicial}`;
+               let E = `${(tiempo - tiempo_inicial) - tiempo_ejecucion}`;
+               let I = ((tiempo_ejecucion) / T).toFixed(2);
+
+               let posicion = proceso.split(' ')[3];
+               
+               resultado.push(`${posicion} ${tiempo} ${T} ${E} ${I}`);
+
+               if (datos.length == 0) {
+                  break;
+               } 
+
+               proceso = datos.shift();
+            } else {
+               tiempo++;
+            }
+         }
+         agregarDatosATabla(resultado);
+         break;
+      default:
+         break;
+   }
 }
 
 let algoritmos = document.querySelectorAll('.algoritmo');
+let btnCorrer = document.querySelector('.btnCorrer');
+let textoAlgoritmo = '';
+let verificarAlgoritmo = false;
 
 algoritmos.forEach(algoritmo => {
     algoritmo.addEventListener('click', cambiarActive);
 });
 
-let btnCorrer = document.querySelector('btnCorrer');
 
 btnCorrer.addEventListener('click', function(e) {
-   
+   Object.values(algoritmo).some((valor) => {
+      if (valor) {
+         verificarAlgoritmo = true;
+      }
+   })
+
+   if (verificarAlgoritmo) {
+      calcularAlgoritmo(textoAlgoritmo);
+   } else {
+      mostrarModal();
+   }
 })
